2ae5155ba7dc3e136887166a78696d41fedeb51f
Detdondon <160354654+Detdondon@users.noreply.github.com>
2026-09-12 15:35:59 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE='pala-v107';
+const CORE=['./','./index.html','./Logo.png','./manifest.webmanifest','./pala-icon.svg','./pala-icon-32.png','./pala-icon-180.png','./pala-icon-192.png','./pala-icon-512.png'];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()));
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key.startsWith('pala-')&&key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));
+});
+
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET'||new URL(event.request.url).origin!==location.origin)return;
+  if(event.request.mode==='navigate'){
+    event.respondWith(fetch(event.request).then(response=>{let copy=response.clone();caches.open(CACHE).then(cache=>cache.put('./index.html',copy));return response}).catch(()=>caches.match('./index.html')));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{let copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response})));
+});
