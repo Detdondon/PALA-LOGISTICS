7b8130fb6cb607651e326aa46e26d8f9a200a296
Detdondon <160354654+Detdondon@users.noreply.github.com>
2026-09-13 19:55:35 +0200

    Load updated gear icon across PALA

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='pala-v160-calendar-toolbar-layout';
-const CORE=['./fonts/inter-400.ttf','./fonts/inter-700.ttf','./','./index.html','./workshop-edit.js?v=3','./calendar-controller.js?v=9','./calendar-toolbar.js?v=1','./Logo.png','./manifest.webmanifest?v=110','./pala-icon.svg?v=110','./pala-icon-32.png?v=110','./pala-icon-180.png?v=110','./pala-icon-192.png?v=110','./pala-icon-512.png?v=110'];
+const CACHE='pala-v161-settings-icon';
+const CORE=['./fonts/inter-400.ttf','./fonts/inter-700.ttf','./','./index.html','./workshop-edit.js?v=3','./calendar-controller.js?v=9','./calendar-toolbar.js?v=1','./settings-icon.js?v=1','./Logo.png','./manifest.webmanifest?v=110','./pala-icon.svg?v=110','./pala-icon-32.png?v=110','./pala-icon-180.png?v=110','./pala-icon-192.png?v=110','./pala-icon-512.png?v=110'];
 
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()));
@@ -20,7 +20,8 @@ async function withAppExtensions(response){
   if(!html.includes('workshop-edit.js'))html=html.replace('</body>','<script src="workshop-edit.js?v=3"></script></body>');
   html=html.replace(/<script\s+src=["']calendar-controller\.js[^"']*["']><\/script>/gi,'');
   html=html.replace(/<script\s+src=["']calendar-toolbar\.js[^"']*["']><\/script>/gi,'');
-  html=html.replace('</body>','<script src="calendar-controller.js?v=9"></script><script src="calendar-toolbar.js?v=1"></script></body>');
+  html=html.replace(/<script\s+src=["']settings-icon\.js[^"']*["']><\/script>/gi,'');
+  html=html.replace('</body>','<script src="calendar-controller.js?v=9"></script><script src="calendar-toolbar.js?v=1"></script><script src="settings-icon.js?v=1"></script></body>');
   const headers=new Headers(response.headers);headers.delete('content-length');
   return new Response(html,{status:response.status,statusText:response.statusText,headers});
 }
