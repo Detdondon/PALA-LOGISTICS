1e46dedbfd2d09d4fb60c237d11a9e01a849bbb9
Detdondon <160354654+Detdondon@users.noreply.github.com>
2026-09-22 22:31:08 +0200

    Bump PALA version to v306

--- app-version.js
+++ app-version.js
@@ -1,7 +1,7 @@
-/* PALA visible app version v305 · isolated from core rendering */
+/* PALA visible app version v306 · isolated from core rendering */
 (()=>{
 'use strict';
-const VERSION='v305';
+const VERSION='v306';
 window.PALA_APP_VERSION=VERSION;
 let tries=0;
 function apply(){
