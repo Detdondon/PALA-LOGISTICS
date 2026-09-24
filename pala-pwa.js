c0553e77b22b45581157f5a86a7d3379e3ea6211
Detdondon <160354654+Detdondon@users.noreply.github.com>
2026-09-24 06:50:39 +0200

    Load PALA v341 service worker

--- pala-pwa.js
+++ pala-pwa.js
@@ -1,9 +1,9 @@
-/* PALA PWA bootstrap v340 */
+/* PALA PWA bootstrap v341 */
 (function(){
   'use strict';
   if(!('serviceWorker'in navigator))return;
   function register(){
-    navigator.serviceWorker.register('./sw.js?v=340',{scope:'./'}).then(reg=>{
+    navigator.serviceWorker.register('./sw.js?v=341',{scope:'./'}).then(reg=>{
       window.__palaServiceWorker=reg;
       reg.update().catch(()=>{});
       if(reg.waiting)reg.waiting.postMessage({type:'SKIP_WAITING'});
